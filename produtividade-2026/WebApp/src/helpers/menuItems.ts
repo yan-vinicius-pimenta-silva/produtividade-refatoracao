b8849d93b4b9da133f3f366a2f27173cbfaf6237
Yan <yan.fedora@gmail.com>
2026-01-14 11:15:49 -0300

    Fix sidebar historico route

--- produtividade-2026/WebApp/src/helpers/menuItems.ts
+++ produtividade-2026/WebApp/src/helpers/menuItems.ts
@@ -43,7 +43,7 @@ export const menuItems: MenuItem[] = [
       {
         label: 'Histórico',
         icon: faChartBar,
-        route: '/produtividade',
+        route: '/produtividade/historico',
       },
       {
         label: 'Parâmetros',
